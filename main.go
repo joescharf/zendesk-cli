570943c4deea0cb6c6fe9aea3fb3556a3b49b584
Joe Scharf <joe@joescharf.com>
2016-11-21 15:01:24 -0700

    Adding ticket id

--- main.go
+++ main.go
@@ -19,8 +19,8 @@ type ZendeskCfg struct {
 func main() {
 
 	var zendeskCfg ZendeskCfg
-	record := make([]string, 8)
-	heading := []string{"created_at", "type", "status", "subject", "requester_id", "submitter_id", "recipient", "description"}
+	record := make([]string, 9)
+	heading := []string{"id", "created_at", "type", "status", "subject", "requester_id", "submitter_id", "recipient", "description"}
 
 	zendeskSettings, err := ioutil.ReadFile("config.yaml")
 	if err == nil {
@@ -46,14 +46,15 @@ func main() {
 
 	tickets, _ := auth.ListTickets()
 	for _, t := range tickets.Tickets {
-		record[0] = t.CreatedAt
-		record[1] = t.Type
-		record[2] = t.Status
-		record[3] = t.Subject
-		record[4] = strconv.Itoa(int(t.RequesterId))
-		record[5] = strconv.Itoa(int(t.SubmitterId))
-		record[6] = t.Recipient
-		record[7] = t.Description
+		record[0] = strconv.Itoa(int(t.Id))
+		record[1] = t.CreatedAt
+		record[2] = t.Type
+		record[3] = t.Status
+		record[4] = t.Subject
+		record[5] = strconv.Itoa(int(t.RequesterId))
+		record[6] = strconv.Itoa(int(t.SubmitterId))
+		record[7] = t.Recipient
+		record[8] = t.Description
 		if err := w.Write(record); err != nil {
 			glog.Fatalln("Error writing record to csv: ", err)
 		}
