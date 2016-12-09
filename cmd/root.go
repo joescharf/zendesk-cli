7f9ed2f071f63a4c0850d2878ac540bf0d31ec31
Joe Scharf <joe@joescharf.com>
2016-12-09 11:19:49 -0700

    Added error checking for config file and error message output

--- cmd/root.go
+++ cmd/root.go
@@ -73,5 +73,8 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	} else {
+		fmt.Println("** Error reading Zendesk config file:", err)
+		os.Exit(1)
 	}
 }
--- main.go
+++ main.go
@@ -19,35 +19,4 @@ func main() {
 	cmd.Execute()
 	fmt.Println()
 
-	// record := make([]string, 9)
-	// heading := []string{"id", "created_at", "type", "status", "subject", "requester_id", "submitter_id", "recipient", "description"}
-
-	// file, err := os.Create("zendesk.csv")
-	// if err != nil {
-	// 	glog.Fatalln("Error creating output file: ", err)
-	// }
-	// defer file.Close()
-
-	// w := csv.NewWriter(file)
-	// if err = w.Write(heading); err != nil {
-	// 	glog.Fatalln("Error writing header to csv: ", err)
-	// }
-
-	// tickets, _ := auth.ListTickets()
-	// for _, t := range tickets.Tickets {
-	// 	record[0] = strconv.Itoa(int(t.Id))
-	// 	record[1] = t.CreatedAt
-	// 	record[2] = t.Type
-	// 	record[3] = t.Status
-	// 	record[4] = t.Subject
-	// 	record[5] = strconv.Itoa(int(t.RequesterId))
-	// 	record[6] = strconv.Itoa(int(t.SubmitterId))
-	// 	record[7] = t.Recipient
-	// 	record[8] = t.Description
-	// 	if err := w.Write(record); err != nil {
-	// 		glog.Fatalln("Error writing record to csv: ", err)
-	// 	}
-	// 	// fmt.Print(t.Subject + " ")
-	// }
-	// w.Flush()
 }
